cacfd35c0235eb76e5ed838cea0a95cb534fe327
esalinas <efsalinas00@gmail.com>
2015-05-18 18:39:38 -0600

    Added pause/start button

--- src/js/handler.js
+++ src/js/handler.js
@@ -3,38 +3,27 @@ function Handler (cb,next) {
 	this.intId;
 	var paused = true;
 	var canvas = document.getElementById('game');
+	var startBtn = document.getElementById('start-btn');
+	var pauseBtn = document.getElementById('pause-btn');
 	canvas.addEventListener('click',function (e) {
 		var i = Math.floor(e.offsetY/(20+1));
 		var j = Math.floor(e.offsetX/(20+1));
 		cb({i:i,j:j});
 	},false);
 
-	document.addEventListener('keydown',function(e){
-		switch(e.which){
-			case 39:
-      		case 68:
-		        //console.log("Right action");
-		       	//code here
+	
+	startBtn.addEventListener('click',function(){
+		if(paused){
+    		console.log("GAME RESUMED");
+    		self.intId = setInterval(next,350);
+    		paused = false;
+	    }
+	},false);
 
-	        	//next
-	        	console.log(self.intId," self.intId");
-	        	if(paused){
-	        		console.log("GAME RESUMED");
-	        		self.intId = setInterval(next,350);
-	        		paused = false;
-	        	}
-	        	break;
-			case 37:
-			case 65:
-				//console.log("Left action");
-				//code here
-				console.log("PAUSE");
-				clearInterval(self.intId);
-				paused = true;
-				break;
-	        default:
-	        	break;
-		}
+	pauseBtn.addEventListener('click',function(){
+		console.log("PAUSE");
+		clearInterval(self.intId);
+		paused = true;
 	},false);
 }
 
